f9f9411413ffa5284db59c75c685d12aaa86c8da
Philipp Page <github@philipp-page.de>
2022-05-23 15:47:28 +0200

    Add tensorflow

--- is3_broker_rl/utils.py
+++ is3_broker_rl/utils.py
@@ -4,4 +4,8 @@ import is3_broker_rl
 
 
 def get_root_path() -> Path:
+    """
+    This is the path to the root directory containing the pyproject.toml file.
+    :return: Path to the root directory
+    """
     return Path(is3_broker_rl.__file__).parent.parent
