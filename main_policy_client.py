f980d5d45f66c618f76de230d3ac377be1852a4b
Philipp Page <github@philipp-page.de>
2022-08-13 11:55:33 +0200

    Remove WholesaleController from main_policy_client.py

--- main_policy_client.py
+++ main_policy_client.py
@@ -5,7 +5,6 @@ import dotenv
 import ray
 
 from is3_broker_rl.api.consumption_tariff_controller import ConsumptionTariffController
-from is3_broker_rl.api.wholesale_controller import WholesaleController
 from is3_broker_rl.conf import setup_logging
 
 
@@ -19,7 +18,7 @@ def main() -> None:
 
     log.info("Starting policy client API ...")
     ConsumptionTariffController.deploy()  # type: ignore
-    WholesaleController.deploy()  # type: ignore
+    # WholesaleController.deploy()  # type: ignore
 
     # We keep this alive until terminated
     while True:
